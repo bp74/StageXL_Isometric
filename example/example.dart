97dac0d2ec81e88966d9d7fd2f9291c08907c6ef
Bernhard <bernhard.robert.pichler@gmail.com>
2013-07-27 20:54:33 +0200

    added IsoGrid to example

--- example/example.dart
+++ example/example.dart
@@ -14,8 +14,8 @@ void main() {
   var background = new Bitmap(new BitmapData(800,600, false, 0x869ca7));
   stage.addChild(background);
 
-  var app = new IsoApplication1();
-  //var app = new IsoApplication2();
+  //var app = new IsoApplication1();
+  var app = new IsoApplication2();
   app.x = 400;
   app.y = 200;
   stage.addChild(app);
@@ -52,11 +52,17 @@ class IsoApplication2 extends Sprite {
       new SolidColorFill(0xff0000, .5),
       new SolidColorFill(0x00ff00, .5),
       new SolidColorFill(0x0000ff, .5)];
-    box.setSize(25, 30, 40);
+    box.setSize(25, 50, 40);
     box.moveTo(0, 0, 0);
+
+    var grid = new IsoGrid();
+    grid.showOrigin = true;
+    grid.setGridSize(10, 10);
+    grid.moveTo(0, 0, 0);
 
     var scene = new IsoScene();
     scene.hostContainer = this;
+    scene.addChild(grid);
     scene.addChild(box);
     scene.isoRender();
   }
